c6d879374e24d420dba5eb7609a824a09f2c7d58
Kramst <hhalooven@gmail.com>
2020-08-24 20:08:59 +0300

    added card value calculating function

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ const getCard = player =>{
         .then(card => {
             let points = document.querySelector('.points')
             points = parseInt(points.innerHTML, 10)
-            if(card.value === 'ACE'){
-                points + 11 > 21 ? points += 1 : points += 11
-                pile.push(card.value)
-            }
-            else isNaN(+card.value) ? points += 10 : points += (+card.value)
+            points = calcValue(card, points)
             document.querySelector('.points').innerHTML = points
         })
+}
+
+const calcValue = (card, points) =>{
+    if(card.value === 'ACE'){
+        points + 11 > 21 ? points += 1 : points += 11
+        pile.push(card.value)
+    }
+    else isNaN(+card.value) ? points += 10 : points += (+card.value)
+    return(points)
 }
